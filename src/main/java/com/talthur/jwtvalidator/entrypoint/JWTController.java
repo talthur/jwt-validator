3915511b0bf832179f8ce3d5300c3a7614bdc126
talthur <talthur@linux.myguest.virtualbox.org>
2024-02-26 11:41:28 -0300

    adiciona docker file e compose para subir aplicacao

--- src/main/java/com/talthur/jwtvalidator/entrypoint/JWTController.java
+++ src/main/java/com/talthur/jwtvalidator/entrypoint/JWTController.java
@@ -1,6 +1,8 @@
 package com.talthur.jwtvalidator.entrypoint;
 
 import com.talthur.jwtvalidator.usecase.JWTValidatorUseCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/jwt")
 public class JWTController {
 
+    Logger logger = LoggerFactory.getLogger(JWTController.class);
+
     private final JWTValidatorUseCase jwtValidatorUseCase;
 
     public JWTController(JWTValidatorUseCase jwtValidatorUseCase) {
@@ -19,7 +23,9 @@ public class JWTController {
 
     @PostMapping("/validate")
     public ResponseEntity<JWTValidatedDTO> validate(@RequestBody JWTDTO jwtDto) {
+        logger.info("Validating JTW: {}", jwtDto.token());
         Boolean isValid = jwtValidatorUseCase.validateJWT(jwtDto.token());
+        logger.info("Validation result: {} for JWT token: {}",isValid, jwtDto.token());
         return ResponseEntity.ok(new JWTValidatedDTO(jwtDto.token(), isValid));
     }
 
